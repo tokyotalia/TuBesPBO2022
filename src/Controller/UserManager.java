71f599affe35268306c89455df9064fdc09fd6cb
Darrel <anselmusdarrel@gmail.com>
2022-07-06 23:19:08 +0700

    Fix user manager

--- src/Controller/UserManager.java
+++ src/Controller/UserManager.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Controller;
 
 import Model.User;
 
-/**
- *
- * @author Sebastian Berto
- */
 public class UserManager {
     static UserManager instance;
     private User user;
